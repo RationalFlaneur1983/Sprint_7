11ca1fbdba31d2d33b4feca7f54d10b3eff40ad9
RationalFlaneur1983 <ashikhmatov@gmail.com>
2025-01-19 10:58:26 +0600

    Moved for loop from test_orders_list to helpers

--- tests/test_orders_list.py
+++ tests/test_orders_list.py
@@ -1,6 +1,5 @@
 import requests
 import allure
-from user_data import OrderData
 from helpers import OrderHelper
 from urls import Urls
 
@@ -9,15 +8,12 @@ class TestOrdersListGet:
     @allure.title('Проверка что при запросе списка заказов в теле ответа возвращается список')
     @allure.description('Создаем четыре заказа из user_data с разными параметрами, выводим список на экран. Проверяются код и тело ответа.')
     def test_orders_get_list(self):
-        for order_data in [
-            OrderData.order_data_grey,
-            OrderData.order_data_black,
-            OrderData.order_data_two_colors,
-            OrderData.order_data_no_color
-        ]:
-            OrderHelper.create_orders(order_data)
+
+        order_data_list = OrderHelper.get_predefined_order_data()
+        add_new_orders = OrderHelper.create_orders(order_data_list)
 
         response = requests.get(Urls.url_orders_create)
         assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}. Response: {response.text}"
+
         response_json = response.json()
         assert isinstance(response_json.get('orders'), list), f"'orders' is not a list. Response: {response_json}"
